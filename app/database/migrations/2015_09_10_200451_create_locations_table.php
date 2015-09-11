c34b5e045b6cd6e258a947a467de0bdf4fb61d9b
Jordan Sandoval <sndvljordan@aol.com>
2015-09-11 10:29:28 -0500

    brought back locations and bower

---
+++ app/database/migrations/2015_09_10_200451_create_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+class CreateLocationsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('location_name');
+            $table->string('location_street');
+            $table->string('location_city');
+            $table->string('location_state');
+            $table->string('location_zip');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations');
+    }
+}
